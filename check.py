0199a59da90af0354d2120ce25192420b098d5b7
OdetQuimper <fujan03@googlemail.com>
2026-06-22 16:30:14 +0200

    Update check.py

--- check.py
+++ check.py
@@ -6,15 +6,6 @@ with sync_playwright() as p:
 
     page.goto("https://braucheklima.de/", wait_until="networkidle")
 
-    print("Links:")
-    links = page.locator("button, a")
-
-    for i in range(min(200, links.count())):
-        try:
-            text = links.nth(i).inner_text().strip()
-            if text in ["10 km", "25 km", "50 km", "100 km", "200 km"]:
-                print(text)
-        except:
-            pass
+    print(page.content()[:10000])
 
     browser.close()
