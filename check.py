71fc8e0731b8b1af4fc9adc81f953a7e3f3e7c95
OdetQuimper <fujan03@googlemail.com>
2026-06-22 16:15:18 +0200

    Update check.py

--- check.py
+++ check.py
@@ -1,4 +1,7 @@
 from playwright.sync_api import sync_playwright
+import requests
+
+TOPIC = "porty"
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
@@ -6,6 +9,15 @@ with sync_playwright() as p:
 
     page.goto("https://braucheklima.de/", wait_until="networkidle")
 
-    print(page.locator("body").inner_text()[:5000])
+    text = page.locator("body").inner_text()
+
+    if "mit PortaSplit\n0" not in text:
+        requests.post(
+            f"https://ntfy.sh/{TOPIC}",
+            data="🚨 Es gibt aktuell PortaSplit-Filialen!".encode("utf-8"),
+            headers={"Title": "PortaSplit Alarm"}
+        )
 
     browser.close()
+
+print("Fertig")
