616cb68c5382071842ad9dac139fc58c6885e133
OdetQuimper <fujan03@googlemail.com>
2026-06-22 15:49:03 +0200

    Update check.py

--- check.py
+++ check.py
@@ -1,12 +1,23 @@
+from playwright.sync_api import sync_playwright
 import requests
 
 TOPIC = "porty"
 
-# Testnachricht senden
-requests.post(
-    f"https://ntfy.sh/{TOPIC}",
-    data="GitHub-Überwachung erfolgreich eingerichtet!".encode("utf-8"),
-    headers={"Title": "PortaSplit Monitor"}
-)
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
 
-print("Nachricht gesendet")
+    page.goto("https://braucheklima.de/", wait_until="networkidle")
+
+    text = page.locator("body").inner_text()
+
+    if "Verfügbar" in text:
+        requests.post(
+            f"https://ntfy.sh/{TOPIC}",
+            data="Test: Das Wort 'Verfügbar' wurde auf der Seite gefunden.".encode("utf-8"),
+            headers={"Title": "PortaSplit Test"}
+        )
+
+    browser.close()
+
+print("Fertig")
